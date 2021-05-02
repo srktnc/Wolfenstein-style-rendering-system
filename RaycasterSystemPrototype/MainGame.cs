3ceac849127f9858c5cc2f94b3205ff450b0e691
Serkan Tunc <serkantuncofficial@gmail.com>
2021-05-02 16:02:50 +0300

    Initialize commit

---
+++ RaycasterSystemPrototype/MainGame.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Diagnostics;
+
+namespace RaycasterSystemPrototype
+{
+    public class MainGame : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        
+        public MainGame()
+        {
+            this._graphics = new GraphicsDeviceManager(this);
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
